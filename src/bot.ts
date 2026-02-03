6b3f05f5e8393e1ab680f6402ab15b63f0169f5c
Yevhenii Pashko <y.pashcode@gmail.com>
2026-02-03 18:18:29 +0200

    refactor(app): bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.BOT_TOKEN) throw new Error('❌ BOT_TOKEN not found');
+if (!process.env.BOT_TOKEN) throw new Error('❌ BOT_TOKEN не найден в переменных окружения');
 
 export const bot = new Bot(process.env.BOT_TOKEN);
-
-console.log('✅ Bot instance created');
